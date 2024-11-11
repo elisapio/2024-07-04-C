d4c5c2fd94662d6c3c7b8fc82457b05cb406e41d
elisapio <s298197@studenti.polito.it>
2024-11-11 09:46:47 +0100

    tema_ufo c

--- model/modello.py
+++ model/modello.py
@@ -1,6 +1,113 @@
+import copy
+
 from database.DAO import DAO
 import networkx as nx
 
+from model.sighting import Sighting
+
+
 class Model:
     def __init__(self):
-        pass
+        self._grafo = nx.DiGraph()
+        self._nodes = []
+        self._cammino_ottimo = []
+        self._score_ottimo = 0
+        self._occorrenze_mese = dict.fromkeys(range(1, 13), 0)
+
+    def get_years(self):
+        return DAO.get_years()
+
+    def get_shapes_year(self, year: int):
+        return DAO.get_shapes_year(year)
+
+    def create_graph(self, year: int, shape: str):
+        self._grafo.clear()
+        self._nodes = DAO.get_nodes(year, shape)
+        self._grafo.add_nodes_from(self._nodes)
+
+        # calcolo degli edges in modo programmatico
+        for i in range(0, len(self._nodes) - 1):
+            for j in range(i + 1, len(self._nodes)):
+                if self._nodes[i].state == self._nodes[j].state and self._nodes[i].longitude < self._nodes[j].longitude:
+                    weight = self._nodes[j].longitude - self._nodes[i].longitude
+                    self._grafo.add_edge(self._nodes[i], self._nodes[j], weight= weight)
+                elif self._nodes[i].state == self._nodes[j].state and self._nodes[i].longitude > self._nodes[j].longitude:
+                    weight = self._nodes[i].longitude - self._nodes[j].longitude
+                    self._grafo.add_edge(self._nodes[j], self._nodes[i], weight= weight)
+
+    def get_top_edges(self):
+        sorted_edges = sorted(self._grafo.edges(data=True), key=lambda edge: edge[2].get('weight'), reverse=True)
+        return sorted_edges[0:5]
+
+
+    def get_nodes(self):
+        return self._grafo.nodes()
+
+    # def get_edges(self):
+    #     return list(self._grafo.edges(data=True))
+
+    def get_num_of_nodes(self):
+        return self._grafo.number_of_nodes()
+
+    def get_num_of_edges(self):
+        return self._grafo.number_of_edges()
+
+    def cammino_ottimo(self):
+        self._cammino_ottimo = []
+        self._score_ottimo = 0
+        self._occorrenze_mese = dict.fromkeys(range(1, 13), 0)
+
+        for nodo in self._nodes:
+            self._occorrenze_mese[nodo.datetime.month] += 1
+            successivi_durata_crescente = self._calcola_successivi(nodo)
+            self._calcola_cammino_ricorsivo([nodo], successivi_durata_crescente)
+            self._occorrenze_mese[nodo.datetime.month] -= 1
+        return self._cammino_ottimo, self._score_ottimo
+
+    def _calcola_cammino_ricorsivo(self, parziale: list[Sighting], successivi: list[Sighting]):
+        if len(successivi) == 0:
+            score = Model._calcola_score(parziale)
+            if score > self._score_ottimo:
+                self._score_ottimo = score
+                self._cammino_ottimo = copy.deepcopy(parziale)
+        else:
+            for nodo in successivi:
+                # aggiungo il nodo in parziale ed aggiorno le occorrenze del mese corrispondente
+                parziale.append(nodo)
+                self._occorrenze_mese[nodo.datetime.month] += 1
+                # nuovi successivi
+                nuovi_successivi = self._calcola_successivi(nodo)
+                # ricorsione
+                self._calcola_cammino_ricorsivo(parziale, nuovi_successivi)
+                # backtracking: visto che sto usando un dizionario nella classe per le occorrenze, quando faccio il
+                # backtracking vado anche a togliere una visita dalle occorrenze del mese corrispondente al nodo che
+                # vado a sottrarre
+                self._occorrenze_mese[parziale[-1].datetime.month] -= 1
+                parziale.pop()
+
+    def _calcola_successivi(self, nodo: Sighting) -> list[Sighting]:
+        """
+        Calcola il sottoinsieme dei successivi ad un nodo che hanno durata superiore a quella del nodo e che non eccedano
+        il numero massimo di occorrenze per un dato mese.
+        """
+        successivi = self._grafo.successors(nodo)
+        successivi_ammissibili = []
+        for s in successivi:
+            if s.duration > nodo.duration and self._occorrenze_mese[s.datetime.month] < 3:
+                successivi_ammissibili.append(s)
+        return successivi_ammissibili
+
+    @staticmethod
+    def _calcola_score(cammino: list[Sighting]) -> int:
+        """
+        Funzione che calcola il punteggio di un cammino.
+        :param cammino: il cammino che si vuole valutare.
+        :return: il punteggio
+        """
+        # parte del punteggio legata al numero di tappe
+        score = 100 * len(cammino)
+        # parte del punteggio legata al mese
+        for i in range(1, len(cammino)):
+            if cammino[i].datetime.month == cammino[i - 1].datetime.month:
+                score += 200
+        return score
